7f6faa0e70da67ba6fc4fd1bb62fb6643fe5c43d
KwanHyeon Park <hyunlord@naver.com>
2018-05-26 20:43:05 +0900

    Almost Done

--- Schematic/mainwindow.h
+++ Schematic/mainwindow.h
@@ -1,8 +1,26 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <vector>
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QCursor>
+#include <QMessageBox>
+#include <QMouseEvent>
+#include <QGraphicsSceneMouseEvent>
+#include <QPoint>
+#include <QPointF>
+#include <QLine>
+#include <QLineF>
+#include <QKeyEvent>
+#include <QRgb>
+#include <QImage>
+#include <QWidget>
+#include <Queue>
 
+#define INF 987654321
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +33,34 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_EnterButton_clicked();
+    void on_ResetButton_clicked();
+    void on_makeWall_clicked();
+
+    void on_DoorButton_clicked();
+
+    void on_WindowButton_clicked();
+
 private:
+    bool startDrawing = false;
+    int wallDrawing = 0;
+    int wallx1, wally1, wallx2, wally2;
+    int houseWidth, houseHeight;
+    QMessageBox qmb;
     Ui::MainWindow *ui;
+
+    QGraphicsScene *scene;
+    std::vector<QGraphicsLineItem *> experiorWall;
+    std::vector<QGraphicsLineItem *> interiorWall;
+    std::vector<QGraphicsLineItem *> windowList;
+    std::vector<QGraphicsRectItem *> doorList;
+    std::vector<std::vector<QGraphicsLineItem *> > room;
+    void mousePressEvent(QMouseEvent *mouse);
+    void mouseMoveEvent(QMouseEvent *mouseEvent);
+    void mouseDoubleClickEvent(QMouseEvent *event);
+    void keyPressEvent(QKeyEvent* event);
+    void MainWindow::bfs();
 };
 
 #endif // MAINWINDOW_H
